bb2985593cd8c00cfa3e3ed6911f5ce97ca8af2e
tw00089923 <auwit0205@gmail.com>
2018-07-24 21:30:50 +0800

    Initial Project

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask ,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/<int:first>/<int:second>')
+def project(first,second):
+    return " 數字相乘 {}".format(first*second)
+
+@app.route('/<first>/<second>')
+def wrong_page(first,second):
+    return render_template('bad.html',first = first, second = second )
+
+if __name__ == "__main__":
+    app.run(debug=False)
